b2c82bcd16c6913bde665af411dc94685059a322
Tony James <tony.james@cohaesus.co.uk>
2015-07-31 12:07:54 +0100

    Test

--- js/app.js
+++ js/app.js
@@ -1,17 +1,5 @@
-var tonyApp = angular.module('tonyApp',[]);
-
-// tonyApp.controller('MovieController', ['$scope', function($scope) {
-//   $scope.movies = $http({
-//         method: 'JSONP', 
-//         url: 'http://www.myapifilms.com/imdb/top'
-//    });
-
-tonyApp.controller('MoviesCtrl', ['$scope', '$http', function ($scope, $http) { 
-  $scope.movies = [];
-  $http.get({
-  	method: 'JSONP', 
-    url: 'http://jsonplaceholder.typicode.com/photos'})
-    .then(function(response){
-      $scope.movies = response.data.movies;
-    });
-}]) 
+var app = angular.module('myApp', []);
+app.controller('customersCtrl', function($scope, $http) {
+    $http.get("http://www.w3schools.com/angular/customers.php")
+    .success(function(response) {$scope.names = response.records;});
+});
