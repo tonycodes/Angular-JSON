45f29890a3e4a1b518513ae4fc976650300c5502
Tony James <tony.james@cohaesus.co.uk>
2015-07-31 11:39:27 +0100

    Test

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var tonyApp = angular.module('tonyApp',[]);
 tonyApp.controller('MovieController', ['$scope', function($scope) {
   $scope.movies = $http({
         method: 'JSONP', 
-        url: 'http://www.myapifilms.com/imdb/top'
+        url: 'http://www.myapifilms.com/imdb/top';
         console.log($scope.movies);
    });
 }]);
