dedbe655c54299a430e0f2436351a30b6093eff4
Tony James <tony.james@cohaesus.co.uk>
2015-08-13 16:40:43 +0100

    test

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 var app = angular.module('myApp', []);
-app.controller('customersCtrl', function($scope, $http) {
+app.controller('LinkedInController', function($scope, $http) {
     $http.get("http://www.w3schools.com/angular/customers.php")
     .success(function(response) {$scope.names = response.records;});
+});
+app.controller('MeController', function($scope, $http) {
+  $scope.me = {
+    firstName: Tony,
+    lastName: James
+  }
 });
