90fd601ba5534205d4cfa8f521af0ed5777a9352
Tony James <tony.james@cohaesus.co.uk>
2015-09-21 15:36:08 +0100

    Rename

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <meta name="author" content="">
     <link rel="icon" href="../../favicon.ico">
 
-    <title>Tony James - PHP Developer, London</title>
+    <title>Tony James - PHP Developer, London - PHP</title>
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
