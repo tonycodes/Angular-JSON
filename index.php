10479450d49e8a9fede4ece87730c195fe05bc2c
tonycodes <lovejava@gmx.com>
2015-10-19 11:58:22 +0100

    .

--- index.php
+++ index.php
@@ -86,10 +86,6 @@
     </div><!-- /.container -->
 
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-
-
     <script src="../../dist/js/bootstrap.min.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
